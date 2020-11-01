99db3a3924463c3733e4513e52ff858714e24cd6
Aklim21 <59926415+Aklim21@users.noreply.github.com>
2020-11-01 11:49:59 +0000

    commit to master

--- src/components/FormGUI.js
+++ src/components/FormGUI.js
@@ -2,58 +2,60 @@ import React from 'react';
 
 function FormGUI(props){
     return (
-        <main className="formGUI">
-            <form onSubmit={props.handleSubmit}>
-                <input type="text" name="firstName" value={props.data.firstName} placeholder="First Name" onChange={props.changeHandler} /><br />
-                <input type="text" name="lastName" value={props.data.lastName} placeholder="Last Name" onChange={props.changeHandler} /><br />
-                <input type="text" name="age" value={props.data.age} placeholder="Age" onChange={props.changeHandler} /><br />
-            
-                <label> 
-                    <input  
-                        type="radio"  
-                        name="gender" 
-                        value="male" 
-                        checked={props.data.gender === "male"} 
-                        onChange={props.changeHandler} 
-                    /> Male 
-                </label> 
-
-                <label> 
-                    <input  
-                        type="radio"  
-                        name="gender" 
-                        value="female" 
-                        checked={props.data.gender === "female"} 
-                        onChange={props.changeHandler} 
-                    /> Female
-                </label>                    
-                <br />
-                <br />
+        <main className="form">
+            <div className="form-GUI">
+                <form onSubmit={props.handleSubmit}>
+                    <input type="text" name="firstName" value={props.data.firstName} placeholder="First Name" onChange={props.changeHandler} /><br />
+                    <input type="text" name="lastName" value={props.data.lastName} placeholder="Last Name" onChange={props.changeHandler} /><br />
+                    <input type="text" name="age" value={props.data.age} placeholder="Age" onChange={props.changeHandler} /><br />
                 
-                <label>Destination: </label>
-                <select name="destination" value={props.data.destination} onChange={props.changeHandler}>
-                    <option value="London">London</option>
-                    <option value="Sydney">Sydney</option>
-                    <option value="Toronto">Toronto</option>
-                </select>
-                <br />
-                <br/>
-                <label>
-                <input type="checkbox" name="nuts" checked={props.data.nuts} onChange={props.changeHandler}/>
-                Nut Allergy? 
-                </label>
-                <br />
-                <br/>
-                <button>Submit</button>
-            </form>
-            <hr />
-            <h2 >Entered information:</h2>
-            <p>Name: {props.data.firstName} {props.data.lastName}</p>
-            <p>Your age: {props.data.age}</p>
-            <p>Your gender: {props.data.gender}</p>
-            <p>Your destination: {props.data.destination}</p>
-            <p>Your dietary restrictions: </p>
-            <p>Nuts: {props.data.nuts}</p>
+                    <label> 
+                        <input  
+                            type="radio"  
+                            name="gender" 
+                            value="male" 
+                            checked={props.data.gender === "male"} 
+                            onChange={props.changeHandler} 
+                        /> Male 
+                    </label> 
+
+                    <label> 
+                        <input  
+                            type="radio"  
+                            name="gender" 
+                            value="female" 
+                            checked={props.data.gender === "female"} 
+                            onChange={props.changeHandler} 
+                        /> Female
+                    </label>                    
+                    <br />
+                    <br />
+                    
+                    <label>Destination: </label>
+                    <select name="destination" value={props.data.destination} onChange={props.changeHandler}>
+                        <option value="London">London</option>
+                        <option value="Sydney">Sydney</option>
+                        <option value="Toronto">Toronto</option>
+                    </select>
+                    <br />
+                    <br/>
+                    <label>
+                    <input type="checkbox" name="nuts" checked={props.data.nuts} onChange={props.changeHandler}/>
+                    Nut Allergy? 
+                    </label>
+                    <button>Submit</button>
+                </form>
+            </div>
+            <div className="form-report">
+                <hr />
+                <h2 >Entered information:</h2>
+                <p>Name: {props.data.firstName} {props.data.lastName}</p>
+                <p>Your age: {props.data.age}</p>
+                <p>Your gender: {props.data.gender}</p>
+                <p>Your destination: {props.data.destination}</p>
+                <p>Your dietary restrictions: </p>
+                <p>Nuts: {props.data.nuts}</p>
+            </div>
         </main>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import hello from "../images/hello.png"
 
 
 export default function Home() {
     return (
-        <>
+        <h1 style = {{
+            alignContent: "centre",
+            paddingTop: "20px",
+            alignItems: "center",
+            justifyContent: "center",
+        }}>
             <div>
+                <img style={{width: "300"}} src={hello} alt='hello'/>
+                <br/>
                 This is home page.
             </div>
             <div>
                 <Link to="/code/">
                     Go code
                 </Link> 
+
+
             </div>
-        </>
+        </h1>
     )
 }
